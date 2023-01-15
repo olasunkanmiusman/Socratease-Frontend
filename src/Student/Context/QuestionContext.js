6455d9b886b55bdb02e8a2f55ac8db9634950c01
Olasunkanmi Usman <olasunkanmiusman1111@gmail.com>
2023-01-15 21:09:10 +0100

    Added Loader to Student Page

--- src/Student/Context/QuestionContext.js
+++ src/Student/Context/QuestionContext.js
@@ -20,7 +20,7 @@ export const QuestionProvider = ({children}) => {
     const [QuestionTime, setQuestionTime] = useState(0);
     const [showTest, setShowTest] = useState(false);
     const [error, setError] = useState(undefined); 
-    const url = (`https://socratease-backend.onrender.com`); 
+    const url = (`https://socratease-backend.onrender.com`);  
      
 
         const setAnswerOptions = (value) => {
